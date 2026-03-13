bba34e916b926fd2f3b716ed636477fbd2a2289b
igor <igor.mejsner@cyfroweszkoly.pl>
2026-03-13 09:24:23 +0100

    Create function CalculatorPrice

--- RapidPack/MainWindow.axaml.cs
+++ RapidPack/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia.Controls;
 
 namespace RapidPack;
@@ -12,6 +13,29 @@ public partial class MainWindow : Window
 
 public class ParcelCalculator
 {
-    public static void CalculatorPrice()
-    {}
+    public double CalculatorPrice(
+        double height,
+        double width,
+        double length,
+        double weight,
+        bool express,
+        string type)
+    {
+        if (weight > 30)
+        {
+            throw new Exception("Paczka jest zbyt ciężka");
+        }
+
+        double price = type == "paleta" ? 100 : 10;
+
+        price += type == "ostroznie" ? 10 : 0;
+
+        price += weight * 2;
+
+        price *= (height + width + length > 150) ? 1.5 : 1;
+
+        price += express ? 15 : 0;
+
+        return price;
+    }
 }
